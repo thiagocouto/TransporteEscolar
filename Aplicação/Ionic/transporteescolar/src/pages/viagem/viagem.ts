ec59a11f70de7fd8af15f3ca51e665f3f5e1cc52
thiagocouto <thiagocoutoalmeida@gmail.com>
2017-05-24 22:06:51 -0300

    Refatoração

--- Aplicação/Ionic/transporteescolar/src/pages/viagem/viagem.ts
+++ Aplicação/Ionic/transporteescolar/src/pages/viagem/viagem.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { AgmCoreModule } from 'angular2-google-maps/core';
-
 /**
  * Generated class for the Viagem page.
  *
